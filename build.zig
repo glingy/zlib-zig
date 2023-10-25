e66cfad885ca54c9ac23c2613505e541b63a2703
Gregory Ling <0b0100011101101100@gmail.com>
2023-10-25 17:31:33 -0500

    Fixed build.zig

---
+++ build.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "z",
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.linkLibC();
+    lib.addCSourceFiles(srcs, &.{"-std=c89"});
+
+    lib.installHeader("crc32.h", "crc32.h");
+    lib.installHeader("deflate.h", "deflate.h");
+    lib.installHeader("gzguts.h", "gzguts.h");
+    lib.installHeader("inffast.h", "inffast.h");
+    lib.installHeader("inffixed.h", "inffixed.h");
+    lib.installHeader("inflate.h", "inflate.h");
+    lib.installHeader("inftrees.h", "inftrees.h");
+    lib.installHeader("trees.h", "trees.h");
+    lib.installHeader("zconf.h", "zconf.h");
+    lib.installHeader("zlib.h", "zlib.h");
+    lib.installHeader("zutil.h", "zutil.h");
+
+    b.installArtifact(lib);
+}
+
+const srcs = &.{
+    "adler32.c",
+    "compress.c",
+    "crc32.c",
+    "deflate.c",
+    "gzclose.c",
+    "gzlib.c",
+    "gzread.c",
+    "gzwrite.c",
+    "inflate.c",
+    "infback.c",
+    "inftrees.c",
+    "inffast.c",
+    "trees.c",
+    "uncompr.c",
+    "zutil.c",
+};
